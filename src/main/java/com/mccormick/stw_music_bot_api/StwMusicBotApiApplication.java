b98286dd2c98938152c9403231a9376a84e34626
McCormick <bessonoven@mail.ru>
2024-05-10 22:03:28 +0300

    Initialization commit

---
+++ src/main/java/com/mccormick/stw_music_bot_api/StwMusicBotApiApplication.java
@@ -0,0 +1,13 @@
+package com.mccormick.stw_music_bot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StwMusicBotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StwMusicBotApiApplication.class, args);
+	}
+
+}
